8f7fc149c761c480513a9a7ee06c80bbe3dd356f
bulgier <yip_bulgier.0m@icloud.com>
2024-04-02 23:45:29 +0200

    complete cryptodevs dao project

---
+++ hardhat/scripts/deploy.js
@@ -0,0 +1,47 @@
+const hre = require("hardhat");
+
+async function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function main() {
+    const nftContract = await hre.ethers.deployContract("CryptoDevsNFT");
+    await nftContract.waitForDeployment();
+    console.log("CryptoDevsNFT deployed to:", nftContract.target);
+
+    const fakeNftMarketplaceContract = await hre.ethers.deployContract("FakeNFTMarketplace");
+    await fakeNftMarketplaceContract.waitForDeployment();
+    console.log("FakeNftMarketplace deployed to:", fakeNftMarketplaceContract.target);
+
+    const amount = hre.ethers.parseEther("0.5");
+    const daoContract = await hre.ethers.deployContract("CryptoDevsDAO", 
+    [fakeNftMarketplaceContract.target, nftContract.target,], 
+    {value: amount,});
+    await daoContract.waitForDeployment();
+    console.log("CryptoDevsDAO deployed to:", daoContract.target);
+
+    await sleep(30 * 1000);
+
+    await hre.run("verify:verify", {
+        address: nftContract.target,
+        constructorArguments: [],
+    });
+
+    await hre.run("verify:verify", {
+        address: fakeNftMarketplaceContract.target,
+        constructorArguments: [],
+    });
+
+    await hre.run("verify:verify", {
+        address: daoContract.target,
+        constructorArguments: [
+            fakeNftMarketplaceContract.target,
+            nftContract.target,
+        ]
+    });
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
